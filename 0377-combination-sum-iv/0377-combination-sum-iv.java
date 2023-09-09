cc8bd1a44c06ff1ff4983981873588f3bd8d30b3
Jackie Jiacheng Chen <107894521+JackieChennn@users.noreply.github.com>
2023-09-08 23:50:48 -0400

    Time: 1 ms (72.35%), Space: 40.2 MB (19.48%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,6 +1,5 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
-        Arrays.sort(nums);
         int n = nums.length;
         int[] dp = new int[target + 1];
         dp[0] = 1;
@@ -8,8 +7,6 @@ class Solution {
             for (int num : nums) {
                 if (i >= num) {
                     dp[i] += dp[i - num];
-                } else {
-                    break;
                 }
             }
         }
