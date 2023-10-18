ee1045b4b56b5e01cf42d14de5a16ce9c3a54503
Jackie Jiacheng Chen <107894521+JackieChennn@users.noreply.github.com>
2023-10-18 11:36:23 -0700

    Time: 59 ms (79.72%), Space: 18.2 MB (40.45%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        c = collections.Counter(nums)
+        for k,v in c.items():
+            if v == 1:
+                return k
+        return 0
