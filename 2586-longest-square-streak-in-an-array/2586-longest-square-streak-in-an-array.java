e4931061003743e1937c3b81f1ff924497853051
Jackie Jiacheng Chen <107894521+JackieChennn@users.noreply.github.com>
2024-10-28 11:34:21 -0700

    Time: 57 ms (31.25%), Space: 56.7 MB (93.06%) - LeetHub

---
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestSquareStreak(int[] nums) {
+        // last number -> streak
+        Map<Integer, Integer> hm = new HashMap<>();
+        Arrays.sort(nums);
+        int root = 0;
+        for (int num : nums) {
+            root = (int) Math.sqrt(num);
+            if (root * root == num && hm.containsKey(root)) {
+                hm.put(num, hm.get(root) + 1);
+                hm.remove(root);
+            } else if (!hm.containsKey(num)){
+                hm.put(num, 1);
+            }
+        }
+        int maxLen = -1;
+        for (int len : hm.values()) {
+            maxLen = Math.max(maxLen, len);
+        }
+        return maxLen == 1 ? -1 : maxLen;
+    }
+}
