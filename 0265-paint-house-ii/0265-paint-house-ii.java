9c55961acb8bed581d2bca474085ae70428aed43
Jackie Jiacheng Chen <107894521+JackieChennn@users.noreply.github.com>
2024-10-29 11:05:11 -0700

    Time: 5 ms (6.25%), Space: 43.9 MB (87.73%) - LeetHub

--- 0265-paint-house-ii/0265-paint-house-ii.java
+++ 0265-paint-house-ii/0265-paint-house-ii.java
@@ -3,14 +3,22 @@ class Solution {
         int n = costs.length;
         int k = costs[0].length;
         for (int i = n - 2; i >= 0; --i) {
+            int minColor = -1;
+            int secondMinColor = -1;
             for (int j = 0; j < k; ++j) {
-                int min = Integer.MAX_VALUE;
-                for (int h = 0; h < k; ++h) {
-                    if (h != j) {
-                        min = Math.min(min, costs[i + 1][h]);
-                    }
+                if (minColor == -1 || costs[i + 1][j] < costs[i + 1][minColor]) {
+                    secondMinColor = minColor;
+                    minColor = j;
+                } else if (secondMinColor == -1 || costs[i + 1][j] < costs[i + 1][secondMinColor]) {
+                    secondMinColor = j;
                 }
-                costs[i][j] += min;
+            }
+            for (int j = 0; j < k; ++j) {
+                if (j == minColor) {
+                    costs[i][j] += costs[i + 1][secondMinColor];
+                } else {
+                    costs[i][j] += costs[i + 1][minColor];
+                }
             }
         }
         return IntStream.of(costs[0]).min().getAsInt();
